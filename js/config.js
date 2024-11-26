fcd413a8500a24d7ecf84855e90135f64f82756d
murhalidinov <146174843+murhalidinov@users.noreply.github.com>
2024-11-26 13:25:54 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-2_SUjmNdpu3
+', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '49dl1c6lnbmjtrhmcd6c57v2kj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
